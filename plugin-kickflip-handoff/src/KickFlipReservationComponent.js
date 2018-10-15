60e827cf023515df64cf99a27a0ccf2bd9d7ab3e
Devin Rader <devin@twilio.com>
2018-10-15 08:01:28 -0700

    Update bindings

--- plugin-kickflip-handoff/src/KickFlipReservationComponent.js
+++ plugin-kickflip-handoff/src/KickFlipReservationComponent.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-
 const taskListStyles = {
   padding: '10px',
   margin: '0px',
+  color: '#fff',
+  background: '#000'
 };
 
 const KickFlipReservationComponent = ({ tasks, selectedTaskSid }) => {
@@ -22,18 +23,23 @@ const KickFlipReservationComponent = ({ tasks, selectedTaskSid }) => {
 
     if (task) {
         let taskAttributes = task.reservation.task.attributes;
+        let firstName = taskAttributes.autopilot.memory['twilio.collected_data'][0].answers[0].answer;
+        let reservationDate = taskAttributes.autopilot.memory['twilio.collected_data'][0].answers[1].answer;
+        let reservationTime = taskAttributes.autopilot.memory['twilio.collected_data'][0].answers[2].answer;
+        let partySize = taskAttributes.autopilot.memory['twilio.collected_data'][0].answers[3].answer;
+
         return <div style={taskListStyles}>
             <h1>CURRENT TASK</h1>
             <p>{taskAttributes.title}</p>
             <h1>DATA COLLECTED</h1>
             <h2>FIRST NAME</h2>
-            <p></p>
+            <p>{firstName}</p>
             <h2>RESERVATION DATE</h2>
-            <p></p>
+            <p>{reservationDate}</p>
             <h2>RESERVATION TIME</h2>
-            <p></p>
+            <p>{reservationTime}</p>
             <h2>PARTY SIZE</h2>
-            <p></p>
+            <p>{partySize}</p>
         </div>;
     } else {
         return <div style={taskListStyles}>
